ce4b13c9ff38e544fd912d987eec7f0c946778f5
nguyen-william93 <=>
2021-10-09 17:03:52 -0500

    successfully set up heroku, added a query search for ID

--- server.js
+++ server.js
@@ -15,6 +15,20 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result){
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+function findById (id, animalsArr){
+    const result = animalsArr.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 function filterByQuery (query, animalsArr){
     let personalityTraitArr = [];
     let filteredResults = animalsArr;

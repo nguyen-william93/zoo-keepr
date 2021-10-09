d94c8cc096d6a19bdaba6fd84b142574bb6b4e6e
nguyen-william93 <=>
2021-10-09 16:24:15 -0500

    added animal json to data, added the route for query, added function filter by query to ret return the animal base on the search query

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const { animals } = require('./data/animals');
+
+app.listen(3001, () => {
+    console.log("server is live on port 3001!");
+});
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+function filterByQuery (query, animalsArr){
+    let personalityTraitArr = [];
+    let filteredResults = animalsArr;
+
+    if (query.personalityTraits){
+        if(typeof query.personalityTraits === 'string'){
+            personalityTraitArr = [query.personalityTraits];
+        } else {
+            personalityTraitArr = query.personalityTraits;
+        }
+
+        personalityTraitArr.forEach(trait => {
+            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
+        });
+    }
+
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+        console.log('hi');
+    }
+    if (query.name){
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
